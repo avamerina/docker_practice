e67ebea11b77152c7b1ea9bc18656f29215b7668
avamerina <avamerina@gmail.com>
2022-08-31 15:18:11 +0600

    build trial 2

--- python_app/main.py
+++ python_app/main.py
@@ -5,7 +5,7 @@ print('hey it\'s a calendar\n')
 year = int(input('enter a year: '))
 month = int(input('enter a month: '))
 
-print('here is your calendar changed second')
+print('here is your calendar changed third')
 print(calendar.month(year, month))
 
 
