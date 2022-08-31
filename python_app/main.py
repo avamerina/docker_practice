4a101613ec60d4f86bf45d9fbb9c85dcbef38509
avamerina <avamerina@gmail.com>
2022-08-31 14:45:29 +0600

    initial commit

---
+++ python_app/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('hey it\'s a calendar\n')
+
+year = int(input('enter a year: '))
+month = int(input('enter a month: '))
+
+print(calendar.month(year, month))
+
+print('bye bitch')
